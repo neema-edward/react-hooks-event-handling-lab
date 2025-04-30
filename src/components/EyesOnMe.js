c4fc08bd140c9a9ba29f9071e58365055a8972f2
Neema <neema.edward@student.moringaschool.com>
2025-04-30 09:05:25 +0300

    commit

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,17 +1,19 @@
 import React from "react";
 
-function EyesOnMe () {
-    function handleFocus () {
-        console.log("Good!");
-    }
+function EyesOnMe() {
+  function handleFocus() {
+    console.log("Good!");
+  }
 
-    function handleBlur () {
-        console.log("Hey! Eyes on me!");
-    }
+  function handleBlur() {
+    console.log("Hey! Eyes on me!");
+  }
 
-    return (
-        <button onFocus={handleFocus} onBlur={handleBlur}>
-            Eyes on me
-        </button>
-    )
+  return (
+    <button onFocus={handleFocus} onBlur={handleBlur}>
+      Eyes on me
+    </button>
+  );
 }
+
+export default EyesOnMe;
